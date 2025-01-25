c23a4462a7ef47d38e4baf60807cc1a9cba6461b
Edinilson Lopez <Edinilson>
2025-01-25 18:15:06 -0300

    Primeiro exercicio

---
+++ exercicio.py
@@ -0,0 +1,6 @@
+#Escreva um programa que soma dois números inteiros 
+# inseridos pelo usuário.
+val1 = input("Digite o primeiro valor:")
+val2 = input("Digite o segundo valor:")
+res = int(val1) + int(val2)
+print(f"O resultado da soma de {val1} e {val2} é {res}")
